ba3035748689fd3341802e3b394c89eff746dc3d
Heritiana Raveloson <heritiana.rav@gmail.com>
2025-07-15 17:46:40 +0300

    :white_check_mark: Add Home layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import About from './components/about/index.tsx'
 import Layout from './components/layout'
+import Home from './components/home/index.tsx'
+import About from './components/about/index.tsx'
 
 function App() {
 
   return (
     <>
       <Layout>
+        <Home />
         <About />
       </Layout>
     </>
