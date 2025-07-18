5b71e3d29e065acade5dddf5bfa98e85d45fd399
Heritiana Raveloson <heritiana.rav@gmail.com>
2025-07-18 12:55:16 +0300

    :white_check_mark: Change header's order

--- src/components/header/data.ts
+++ src/components/header/data.ts
@@ -1,8 +1,8 @@
 export const navigationLinks = [
   { label: "Home", to: "HOME" },
+  { label: "Portfolio", to: "PORTFOLIO" },
+  { label: "Skills", to: "SKILLS" },
   { label: "About", to: "ABOUT" },
   { label: "Formations", to: "FORMATIONS" },
-  { label: "Skills", to: "SKILLS" },
-  { label: "Portfolio", to: "PORTFOLIO" },
   { label: "Contact", to: "CONTACT" },
 ];
