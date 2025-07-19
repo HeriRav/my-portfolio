b6b14a3776c5dc7708c7155bcd2238c985163739
Heritiana Raveloson <heritiana.rav@gmail.com>
2025-07-19 23:00:29 +0300

    :white_check_mark: Add skills section in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Layout from './components/layout'
 import Home from './pages/home/index.tsx'
 import Works from './pages/works/index.tsx'
-// import Skills from './pages/skills/index.tsx'
+import Skills from './pages/skills/index.tsx'
 // import Formations from './pages/formations/index.tsx'
 import About from './pages/about/index.tsx'
 // import Contact from './pages/contact/index.tsx'
@@ -13,7 +13,7 @@ function App() {
       <Layout>
         <Home />
         <Works />
-        {/* <Skills /> */}
+        <Skills />
         {/* <Formations /> */}
         <About />
         {/* <Contact /> */}
