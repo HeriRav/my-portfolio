44ba28454245ab1037b08531bf6212e60d4d0cb0
Heritiana Raveloson <heritiana.rav@gmail.com>
2025-07-20 21:51:19 +0300

    :white_check_mark: Change Formatios section into Resume section

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Layout from './components/layout'
 import Home from './pages/home/index.tsx'
 import Works from './pages/works/index.tsx'
 import Skills from './pages/skills/index.tsx'
-import Formations from './pages/formations/index.tsx'
+import Resume from './pages/resume/index.tsx'
 import About from './pages/about/index.tsx'
 // import Contact from './pages/contact/index.tsx'
 
@@ -15,7 +15,7 @@ function App() {
         <Works />
         <Skills />
         <About />
-        <Formations />
+        <Resume />
         {/* <Contact /> */}
       </Layout>
     </>
