50e0c31053d941e55047f03e7310f31242210963
Heritiana Raveloson <heritiana.rav@gmail.com>
2025-07-20 14:47:24 +0300

    :white_check_mark: Add new section

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Layout from './components/layout'
 import Home from './pages/home/index.tsx'
 import Works from './pages/works/index.tsx'
 import Skills from './pages/skills/index.tsx'
-// import Formations from './pages/formations/index.tsx'
+import Formations from './pages/formations/index.tsx'
 import About from './pages/about/index.tsx'
 // import Contact from './pages/contact/index.tsx'
 
@@ -14,8 +14,8 @@ function App() {
         <Home />
         <Works />
         <Skills />
-        {/* <Formations /> */}
         <About />
+        <Formations />
         {/* <Contact /> */}
       </Layout>
     </>
